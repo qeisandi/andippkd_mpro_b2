c6386cb7c49cf8df118aed0c55fa3960b679b73c
qeisandi <qeis.andi02@gmail.com>
2025-05-19 00:32:46 -0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/andippkd_mpro_b2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.andippkd_mpro_b2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/meet_satu/tugas_satu_flutter.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Tugas1 extends StatelessWidget {
+  const Tugas1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+        backgroundColor: Colors.deepOrangeAccent,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(Icons.account_circle),
+          Text('Nama : Andi', style: TextStyle(fontSize: 25)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.location_on),
+              Text('Jakarta', style: TextStyle(fontSize: 20)),
+            ],
+          ),
+          Text('Seorang pelajar yang sedang belajar Flutter.'),
+        ],
+      ),
+    );
+  }
+}
