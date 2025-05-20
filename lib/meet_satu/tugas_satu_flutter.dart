40564594f27339b7dd6954b4f7e440231c2990b4
qeisandi <qeis.andi02@gmail.com>
2025-05-19 18:36:37 -0700

    tugas flutter

--- lib/meet_satu/tugas_satu_flutter.dart
+++ lib/meet_satu/tugas_satu_flutter.dart
@@ -9,6 +9,13 @@ class Tugas1 extends StatelessWidget {
       appBar: AppBar(
         title: Text('Profile'),
         backgroundColor: Colors.deepOrangeAccent,
+        centerTitle: true,
+      ),
+      drawer: Drawer(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Text('hello')],
+        ),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
@@ -23,7 +30,19 @@ class Tugas1 extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Icon(Icons.location_on, color: Colors.red),
-              Text('Jakarta', style: TextStyle(fontSize: 20)),
+              Text(
+                'Jakarta',
+                style: TextStyle(fontSize: 20, color: Colors.lightBlueAccent),
+              ),
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'About:',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+              ),
             ],
           ),
           Text('Seorang pelajar yang sedang belajar Flutter.'),
